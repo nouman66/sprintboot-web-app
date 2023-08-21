b6c54380987160255bd0616cf4424e260e41a79c
noumanahmed <nouman.ahmed@netsoltech.com>
2023-08-21 07:53:44 +0500

    Adding the code and configurations for the app

---
+++ src/test/java/com/nouman/aws/rdssecrets/RdssecretsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nouman.aws.rdssecrets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdssecretsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
